3b1cbe94baa7e45e1efedfe966dc835f0b01f391
GabrielPayano <gabriel21kun@gmail.com>
2018-12-20 14:34:50 +0100

    Añade mensaje de bienvenida

--- src/tareagitgabrielpayano/TareaGitGabrielPayano.java
+++ src/tareagitgabrielpayano/TareaGitGabrielPayano.java
@@ -15,7 +15,10 @@ public class TareaGitGabrielPayano {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
+        
+        System.out.println("Aplicación de Gabriel Payano");
     }
     
 }
